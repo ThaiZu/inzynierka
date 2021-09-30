2799aa26e51bdcf45088cf2d621c4b35db81e7a8
ThaiZu <31669746+ThaiZu@users.noreply.github.com>
2021-10-01 00:46:50 +0200

    refactor login.inc

--- modules/login/login.inc.php
+++ modules/login/login.inc.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include_once ($_SERVER['DOCUMENT_ROOT']).'/inzynierka/modules/login/login.php';
 
 $login = $_POST['login'];
@@ -7,5 +8,15 @@ $passwd = $_POST['password'];
 $role = new \Login\Login();
 if($role->validate($login, $passwd) == true)
 {
-    echo $role->getRole($login);
+    $_SESSION['user'] = $login;
+    $_SESSION['role'] = $role->getRole($login);
+    $_SESSION['logged'] = true;
+
+
+    if($role->getRole($login) == "ROLE_BOSS")
+    {
+        //echo "Location:{$_SERVER['DOCUMENT_ROOT']}/inzynierka/public/boss/index.php";
+        //header("Location:../../public/boss/index.php");
+        header("Location://{$_SERVER['HTTP_HOST']}/inzynierka/public/boss/index.php");
+    }
 }
