a748ecbc878eb1c3c1d6d64d7a75c26a0a30d900
ThaiZu <31669746+ThaiZu@users.noreply.github.com>
2021-10-01 01:54:50 +0200

    added unit module

---
+++ modules/unit/unit.php
@@ -0,0 +1,27 @@
+<?php
+include_once ($_SERVER['DOCUMENT_ROOT']).'/inzynierka/modules/config/database.php';
+
+
+class Unit extends Category
+{
+    private $db;
+    private $table_name = "unit";
+
+    public $unit_name;
+
+    public function __construct()
+    {
+        $this->db = \Database::getInstance();
+    }
+
+    public function select()
+    {
+        $sql = "SELECT * FROM {$this->table_name}";
+        $query = $this->db->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+        $query->execute();
+        return $query;
+    }
+
+
+
+}
